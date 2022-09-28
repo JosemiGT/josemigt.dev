3d4d326b368bf8bf1a833b8e6711ebb1950ceb08
JosemiGT <JM.GamarroTornay@Outlook.com>
2022-09-28 16:22:15 +0200

    Commit inicial para desarrollo de blog

---
+++ src/assets/blog.js
@@ -0,0 +1,51 @@
+
+export const blog = {
+    nav: null,
+    article: null,
+    close: null,
+    originalTitle: null,
+    originalDescription: null,
+    originalURL: null,
+
+    slug: location => new URL(location).pathname.split('/')[1],
+
+    init: async function(options = {}){
+
+        const {
+            nav = 'nav',
+            close = 'button',
+            article = 'article',
+            feed = '/posts/index.json',
+        } = options
+
+        this.nav = document.querySelector(nav)
+        this.close = document.querySelector(close)
+        this.article = document.querySelector(article)
+
+        const response = await fetch(feed)
+        const json = await response.json()
+        this.posts = json.items
+
+    }
+
+}
+
+window.onload = function () {
+    alert("Hola mundo!")
+}
+
+function createCard(articleTittle, articleSummary){
+    let card = document.createElement('div');
+    card.className = "card";
+
+    let title = document.createElement('h3');
+    title.innerText = articleTittle;
+
+    let summary = document.createElement('p');
+    summary.innerText = articleSummary;
+
+}
+
+// TODO: Establecer fichero donde se reflejará la página de los post, el título, fecha y sus tags. (¿En json?)
+// TODO: Leer este fichero desde el onload y guardar en variable.
+// TODO: Construir divs en el body con la información obtenida.
